537b0de92e275e9f9ac7ba89ecdf0352993083fe
Coffee Bean <beanc904@gmail.com>
2026-04-27 23:36:11 +0800

    Auto Sync (arch): 2026-04-27 23:36:11

--- lua/community.lua
+++ lua/community.lua
@@ -5,7 +5,14 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.rust" },
+  -- Base pack with basedpyright, black and isort
+  { import = "astrocommunity.pack.python.base" },
+  { import = "astrocommunity.pack.python.basedpyright" },
+  { import = "astrocommunity.pack.python.black" },
+  { import = "astrocommunity.pack.python.isort" },
   -- import/override with your plugins folder
   { import = "astrocommunity.recipes.neovide" },
 }
